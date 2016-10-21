a585d4de4d36c0f8ab886a7c19455aea6983af6e
RyanRothweiler <ryanrothweiler@gmail.com>
2016-10-20 22:59:51 -0700

    Messing around, testing what was taught in class

--- HW4/WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ HW4/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Diagnostics;
 
 namespace WebApplication1.Controllers
 {
@@ -12,6 +13,22 @@ namespace WebApplication1.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult PageOne()
+        {
+            string[] data = {Request.RawUrl};
+            ViewBag.data = data;
+            return (View());
+        }
+
+        [HttpPost]
+        public ActionResult PageOne(FormCollection form)
+        {
+            string[] data = {Request.RawUrl};
+            ViewBag.data = data;
+            return (View());
         }
     }
 }

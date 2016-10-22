c7c96ede96426f1be70a3f27b79d4dd8ea7a51ba
RyanRothweiler <ryanrothweiler@gmail.com>
2016-10-21 17:35:23 -0700

    Added working pages, index, page 1, and page 2

--- HW4/WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ HW4/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Diagnostics;
+
 
 namespace WebApplication1.Controllers
 {
@@ -18,16 +20,46 @@ namespace WebApplication1.Controllers
         [HttpGet]
         public ActionResult PageOne()
         {
-            string[] data = {Request.RawUrl};
-            ViewBag.data = data;
+            ViewBag.Rows = 0;
+            ViewBag.Columns = 0;
+
             return (View());
         }
 
         [HttpPost]
         public ActionResult PageOne(FormCollection form)
         {
-            string[] data = {Request.RawUrl};
-            ViewBag.data = data;
+            NameValueCollection pairs = Request.Form;
+            ViewBag.Rows = int.Parse(pairs["Rows"]);
+            ViewBag.Columns = int.Parse(pairs["Columns"]);
+
+            return (View());
+        }
+
+        [HttpGet]
+        public ActionResult PageTwo()
+        {
+            ViewBag.BarValue = 0;
+            string inputVal = Request.QueryString["Value"];
+            if (inputVal != null)
+            {
+                ViewBag.BarValue = int.Parse(inputVal);
+            }
+
+            if (ViewBag.BarValue < 0)
+            {
+                ViewBag.BarValue = 0;
+            }
+            if (ViewBag.BarValue > 100)
+            {
+                ViewBag.BarValue = 100;
+            }
+
+            return (View());
+        }
+
+        public ActionResult PageThree()
+        {
             return (View());
         }
     }

e91b914ab82d95f8336ab029307f55897db8f96b
RyanRothweiler <ryanrothweiler@gmail.com>
2016-11-06 13:09:37 -0800

    Added review forms, viewing selected category, and viewing reviews

--- HW6/HW6/Controllers/HomeController.cs
+++ HW6/HW6/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using HW6.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Linq;
 using System.Web;
@@ -22,30 +23,55 @@ namespace HW6.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            Debug.WriteLine("Doing Get");
+
             ViewBag.categories = BuildCategories();
             return View();
         }
 
         [HttpPost]
-        public ActionResult Index(string inputString)
+        public ActionResult Index(string button)
         {
-            Debug.WriteLine("Doing this! " + inputString);
-            /*
-            List<Product> products = new List<Product>();
+            Debug.WriteLine("Doing Post for " + button);
+
+            if (button == "addreview")
+            {
+                Debug.WriteLine("Going to review form");
+            }
+
+            List<Product> allProducts = new List<Product>();
             foreach (Product prod in prodModel.Products)
             {
-                if (prod.ProductSubcategory.Name == subCat)
+                if (prod.ProductSubcategory != null && 
+                    prod.ProductSubcategory.Name == button)
                 {
                     Debug.WriteLine(prod.ProductSubcategory.Name);
-                    products.Add(prod);
+                    allProducts.Add(prod);
                 }
             }
 
-            ViewBag.products = products;
-            return RedirectToAction("ProductsView");
-            */
+            if (allProducts.Count == 0)
+            {
+                Debug.WriteLine("Go to review page for " + button);
 
-            return View();
+                foreach (Product prod in prodModel.Products)
+                {
+                    if (prod.Name == button)
+                    {
+                        Debug.WriteLine("Going to product view");
+                        ViewBag.product = prod;
+                        return View("ProductReviews", prod.ProductReviews);
+                    }
+                }
+
+                Debug.WriteLine("Could not find that product.");
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.products = allProducts;
+                return View("ProductsView");
+            }
         }
 
         public List<CategoryHolder> BuildCategories()
@@ -64,6 +90,65 @@ namespace HW6.Controllers
                 catHolder.Add(newHolder);
             }
             return (catHolder);
+        }
+
+        public ActionResult ProductsView()
+        {
+            return View("Index");
+        }
+
+        public ActionResult ProductReviews()
+        {
+            Debug.WriteLine("Product reviews GET");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProductReviews(string button)
+        {
+            Debug.WriteLine("Add new review to database");
+
+            ViewBag.categories = BuildCategories();
+            return View();
+        }
+
+        public ActionResult AddReview()
+        {
+            return View(new ProductReview());
+        }
+
+        [HttpPost]
+        public ActionResult AddReview(ProductReview review)
+        {
+            Debug.WriteLine("Adding Review");
+
+            if (ModelState.IsValid)
+            {
+                Debug.WriteLine("Adding Review for " + review.ProductID);
+                foreach (Product prod in prodModel.Products)
+                {
+                    if (prod.ProductID == review.ProductID)
+                    {
+                        review.Product = prod;
+                        prod.ProductReviews.Add(review);
+                        prodModel.SaveChanges();
+                        Debug.WriteLine("ADDED REVIEW");
+                        return View("Index");
+                        break;
+                    }
+                }
+
+                for (int index = 0; index < prodModel.Products.Count<Product>(); index++)
+                {
+                    //if (prodModel.Products[index].ProductID == review.ProductID)
+                    {
+
+                    }
+                }
+            }
+
+            return View(review);
         }
     }
 }

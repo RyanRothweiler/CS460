d7cb014e55dfea78b27bb51d9a6d80c9ac26c6fb
RyanRothweiler <ryanrothweiler@gmail.com>
2016-11-15 22:47:52 -0800

    Setup ajax to controller which retuns json. Created basic graph which uses test data.

---
+++ HW7/HW7/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW7.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.csvRows = new string[] { " \" Date,Temperature \" \n + ", "2008-05-07,75 \n", "2008-05-08,70 \n", "2008-05-09,80 \n" };
+
+            return View();
+        }
+
+        public JsonResult GetStockData()
+        {
+            var data = new
+            {
+                row = "Temp, Another"
+            };
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ HW7/HW7/scripts/StockInfo.js
@@ -0,0 +1,30 @@
+﻿function GetPrices()
+{
+    console.log("Getting the new graph");
+
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "/Home/GetStockData",
+        success: DisplayData
+    });
+
+    g = new Dygraph(
+
+       // containing div
+       document.getElementById("graph"),
+
+       // CSV or path to a CSV file.
+       "Date,Temperature\n" +
+       "2008-05-07,75\n" +
+       "2008-05-08,70\n" +
+       "2008-05-09,80\n"
+
+     );
+}
+
+function DisplayData(data)
+{
+    console.log("Data!");
+    console.log(data.row);
+}

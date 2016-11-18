104e664f1ec30e6bf0cd691b8af20fd0053072aa
RyanRothweiler <ryanrothweiler@gmail.com>
2016-11-18 15:07:00 -0800

    Added client side feature to toggle series of data

--- HW7/HW7/Controllers/HomeController.cs
+++ HW7/HW7/Controllers/HomeController.cs
@@ -23,30 +23,81 @@ namespace HW7.Controllers
             string stockSymbol = Request.QueryString["sym"];
             string responseText = String.Empty;
 
+            string[,] dataArray = new string[100, 100];
+            int x = 0;
+            int y = 0;
+            int colCount = 0;
+
+            string[] DateCol = new string[50];
+            string[] OpenCol = new string[50];
+            string[] HighCol = new string[50];
+            string[] LowCol = new string[50];
+            string[] CloseCol = new string[50];
+
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://chart.finance.yahoo.com/table.csv?s=" + stockSymbol + "&a=9&b=16&c=2016&d=10&e=16&f=2016&g=d&ignore=.csv");
             //HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://finance.yahoo.com/d/quotes.csv?s=" + stockSymbol + "&f=o");
             request.Method = "GET";
             try
             {
-
-
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
                 using (StreamReader sr = new StreamReader(response.GetResponseStream()))
                 {
                     responseText = sr.ReadToEnd();
                 }
 
+                string[] dataRows = responseText.Split('\n');
+                for (int index = 0; index < dataRows.Length; index++)
+                {
+                    string[] cells = dataRows[index].Split(',');
 
+                    for (int cellIndex = 0; cellIndex < cells.Length; cellIndex++)
+                    {
+                        // date
+                        if (cellIndex == 0)
+                        {
+                            DateCol[x] = cells[cellIndex];
+                        }
+                        // open
+                        else if (cellIndex == 1)
+                        {
+                            OpenCol[x] = cells[cellIndex];
+                        }
+                        // high 
+                        else if (cellIndex == 2)
+                        {
+                            HighCol[x] = cells[cellIndex];
+                        }
+                        // low
+                        else if (cellIndex == 3)
+                        {
+                            LowCol[x] = cells[cellIndex];
+                        }
+                        // Close
+                        else if (cellIndex == 4)
+                        {
+                            CloseCol[x] = cells[cellIndex];
+                        }
+                    }
+
+                    x++;
+                }
             }
             catch (Exception e)
             {
                 stockSymbol = "No such symbol.";
             }
 
+
+
             var data = new
             {
                 symbol = stockSymbol,
-                yahooFile = responseText,
+
+                date = DateCol,
+                open = OpenCol,
+                low = LowCol,
+                close = CloseCol,
+                high = HighCol,
             };
 
             return Json(data, JsonRequestBehavior.AllowGet);
--- HW7/HW7/scripts/StockInfo.js
+++ HW7/HW7/scripts/StockInfo.js
@@ -1,4 +1,9 @@
-﻿function GetPrices()
+﻿var dateCol, openCol, highCol, lowCol;
+var openEnabled = true;
+var highEnabled = true;
+var lowEnabled = true;
+
+function GetPrices()
 {
     var symbol = document.getElementById("symbol").value;
 
@@ -11,18 +16,65 @@
     });
 }
 
-function DisplayData(data)
+function ToggleOpen()
 {
-    console.log(data.yahooFile);
+    openEnabled = !openEnabled;
+    DisplayFromGlobal();
+}
+
+function ToggleHigh()
+{
+    highEnabled = !highEnabled;
+    DisplayFromGlobal();
+}
+
+function ToggleLow()
+{
+    lowEnabled = !lowEnabled;
+    DisplayFromGlobal();
+}
+
+
+function BuildData()
+{
+    var assembledData = "";
+    for (var y = 0; y < 24; y++) {
+        assembledData += dateCol[y] + ",";
+
+        if (openEnabled) {
+            assembledData += openCol[y] + ",";
+        }
+        if (highEnabled) {
+            assembledData += highCol[y] + ",";
+        }
+        if (lowEnabled) {
+            assembledData += lowCol[y];
+        }
+
+        assembledData += "\n";
+    }
+    return (assembledData);
+}
+
+function DisplayFromGlobal()
+{
     g = new Dygraph(
-        document.getElementById("graph"), 
-        data.yahooFile, 
+        document.getElementById("graph"),
+        BuildData(),
         {
             xlabel: 'Date',
             ylabel: 'Price',
-            valueRange: [0, 300],
-            height: 700,
         }
     );
     document.getElementById("symbolHeader").innerHTML = data.symbol;
+}
+
+function DisplayData(data)
+{
+    dateCol = data.date;
+    openCol = data.open;
+    highCol = data.high;
+    lowCol = data.low;
+    
+    DisplayFromGlobal();
 }
